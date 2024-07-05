d149fcc131daedeea7fe672239888b590b2413e9
jericokatong <jericokatong@gmail.com>
2024-07-05 14:33:55 +0800

    update package.json configuration

--- database/index.js
+++ database/index.js
@@ -2,9 +2,16 @@ const mongoose = require("mongoose");
 
 const { dbHost, dbName, dbPort, dbUser, dbPass } = require("../config");
 
+// mongoose
+//   .connect(
+//     `mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}?authSource=admin`
+//   )
+//   .then(() => console.log("Connected to database"))
+//   .catch((error) => console.log(error));
+
 mongoose
   .connect(
-    `mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}?authSource=admin`
+    `mongodb+srv://${dbUser}:${dbPass}@cluster0.qftwgl1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
   )
   .then(() => console.log("Connected to database"))
   .catch((error) => console.log(error));
